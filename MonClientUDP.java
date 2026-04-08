f34f0f3fb951febf61f1cdace2255d6501566692
Sluuub <nom1@email.com>
2026-04-08 14:17:52 +0200

    création de la classe MonClientUDP

---
+++ MonClientUDP.java
@@ -0,0 +1,38 @@
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
+public class MonClientUDP {
+    
+    public  static void main(String[] args)
+    {
+        try
+        {
+            // 1 - Création du canal
+            DatagramSocket socketClient = new DatagramSocket();
+            InetAddress adresseClient = InetAddress.getByName("localhost");
+            byte[] envoyees; //tampon d'émission
+            byte[] recues = new byte[1024]; // tampon de réception
+            // 2 - émettre
+            String message = "Salve !";
+            envoyees = message.getBytes();
+            DatagramPacket messageEnvoye = new DatagramPacket(envoyees, envoyees.length, adresseClient, 5555);
+            socketClient.send(messageEnvoye);
+            // 3 - Recevoir
+            DatagramPacket paquetRecu = new DatagramPacket(recues, recues.length);
+            socketClient.receive(paquetRecu);
+            String reponse = new String(paquetRecu.getData(), 0, paquetRecu.getLength());
+            System.out.println("Depuis le serveur" + reponse);
+            //4 - Libérer le canal
+            socketClient.close();
+        }
+        catch(Exception e)
+        {
+            System.err.println(e);
+
+        }
+    }
+
+
+
+}
